a45fd9043e0b3c3c834c5cffa1d32adb1dca08d1
Lee.Hyunsup <clocktree@live.co.kr>
2020-10-19 19:16:02 +0900

    20201019_15
    - 수정.

--- js/move.js
+++ js/move.js
@@ -136,8 +136,6 @@ $(function(){
 			title_text = 'main';
 		}else if (split_url=='sub1.html'){
 			title_text = 'introduce';
-			// contents_head='소개페이지 콘텐츠 묶음';
-			// contents_title='숙련도&소개';
 		}else if (split_url=='sub2.html'){
 			title_text = 'web&app';
 		}else if (split_url=='sub3.html'){
@@ -273,20 +271,29 @@ $(function(){
 		function cover_start(){
 			footer_creat();
 			header_creat();
+			
 			if (split_url=='index.html'){
+				contents_head='메인페이지 콘텐츠 묶음';
 				mainSommon();
 			}else if (split_url=='sub1.html'){
 				$('.portfolio_box').html('');
+				contents_head='소개페이지 콘텐츠 묶음';
+				$('.section-heading').html(contents_head);
 				$('.article-summon-wrap').load(portfolioindex_url+'/sub/sub1.html .article-main-scrollall');
 			}else if (split_url=='sub2.html'){
+				contents_head='웹작업페이지 콘텐츠 묶음';
 				webSommon();
 			}else if (split_url=='sub3.html'){
+				contents_head='동영상작업페이지 콘텐츠 묶음';
 				movieSommon();
 			}else if (split_url=='sub4.html'){
+				contents_head='플래시작업페이지 콘텐츠 묶음';
 				flashSommon();
 			}else if (split_url=='sub5.html'){
+				contents_head='랜더링및편집작업페이지 콘텐츠 묶음';
 				imageSommon();
 			}
+			$('.section-heading').html(contents_head);
 		};
 		cover_start();
 	})
@@ -529,6 +536,8 @@ $(function(){
 			{
 				nav_num = 1;
 				$('.portfolio_box').html('');
+				contents_head='소개페이지 콘텐츠 묶음';
+				$('.section-heading').html(contents_head);
 				$('.article-summon-wrap').load(portfolioindex_url+'/sub/sub'+nav_num+'.html .article-main-scrollall');
 			}else if (this==document.getElementById('topmenu1-list-span2'))
 			{
@@ -584,6 +593,8 @@ $(function(){
 			{
 				nav_num = 1;
 				$('.portfolio_box').html('');
+				contents_head='소개페이지 콘텐츠 묶음';
+				$('.section-heading').html(contents_head);
 				$('.article-summon-wrap').load(portfolioindex_url+'/sub/sub'+nav_num+'.html .article-main-scrollall');
 			}else if (this==document.getElementById('list-ul-li2'))
 			{
@@ -632,8 +643,6 @@ $(function(){
 		}
 	};
 	function mainSommon(){
-		contents_head='메인페이지 콘텐츠 묶음';
-		contents_title='환영합니다.';
 		box_maker();
 		$.getJSON(jsonFrame_data, function(data){
 			$.each(data, function(I, item){
@@ -663,11 +672,9 @@ $(function(){
 			});
 		});
 		$('.section-heading').html(contents_head);
-		$('.main-welcolme-summon').find('h2').html(contents_title);
+		$('.main-welcolme-summon').find('h2').html('환영합니다.');
 	};
 	function webSommon(){
-		contents_head='웹작업페이지 콘텐츠 묶음';
-		contents_title='웹&앱 퍼블리싱';
 		box_maker();
 		$.getJSON(jsonFrame_data, function(data){
 			$.each(data, function(I, item){
@@ -697,11 +704,9 @@ $(function(){
 			});
 		});
 		$('.section-heading').html(contents_head);
-		$('.main-welcolme-summon').find('h2').html(contents_title);
+		$('.main-welcolme-summon').find('h2').html('웹&앱 퍼블리싱');
 	};
 	function movieSommon(){
-		contents_head='동영상작업페이지 콘텐츠 묶음';
-		contents_title='동영상 편집작업';
 		box_maker();
 		$.getJSON(jsonFrame_data, function(data){
 			$.each(data, function(I, item){
@@ -731,11 +736,9 @@ $(function(){
 			});
 		});
 		$('.section-heading').html(contents_head);
-		$('.main-welcolme-summon').find('h2').html(contents_title);
+		$('.main-welcolme-summon').find('h2').html('동영상 편집작업');
 	};
 	function flashSommon(){
-		contents_head='플래시작업페이지 콘텐츠 묶음';
-		contents_title='플래시';
 		box_maker();
 		$.getJSON(jsonFrame_data, function(data){
 			$.each(data, function(I, item){
@@ -765,11 +768,9 @@ $(function(){
 			});
 		});
 		$('.section-heading').html(contents_head);
-		$('.main-welcolme-summon').find('h2').html(contents_title);
+		$('.main-welcolme-summon').find('h2').html('플래시');
 	};
 	function imageSommon(){
-		contents_head='랜더링및편집작업페이지 콘텐츠 묶음';
-		contents_title='2D랜더링&편집작업';
 		box_maker();
 		$.getJSON(jsonFrame_data, function(data){
 			$.each(data, function(I, item){
@@ -799,7 +800,7 @@ $(function(){
 			});
 		});
 		$('.section-heading').html(contents_head);
-		$('.main-welcolme-summon').find('h2').html(contents_title);
+		$('.main-welcolme-summon').find('h2').html('2D랜더링&편집작업');
 	};
 	/*------------------------------------------------------------------*/
 
@@ -830,6 +831,8 @@ $(function(){
 				$('#slide-wrap').css({'display':'none'})
 				$('#slide-wrap-i').css({'display':'block'})
 				$('.portfolio_box').html('');
+				contents_head='소개페이지 콘텐츠 묶음';
+				$('.section-heading').html(contents_head);
 				$('.article-summon-wrap').load(portfolioindex_url+'/sub/sub1.html .article-main-scrollall')
 				$('.scr-index-box').load(portfolioindex_url+'/sub/sub1.html .index-btn-wrap')
 			}else if(location.href==portfolioindex_url+'/sub/sub2.html'){
