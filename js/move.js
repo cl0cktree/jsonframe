bde9016ee6e91d25ee4817b18bfda00ebfea10b8
Lee.Hyunsup <clocktree@live.co.kr>
2020-10-19 17:10:15 +0900

    20201019_6
    - 수정.

--- js/move.js
+++ js/move.js
@@ -609,8 +609,9 @@ $(function(){
 			box_maker();
 			
 			$.getJSON(jsonFrame_data, function(data){
-				// function contents_main(){
+				function contents_main(){
 					$.each(data, function(I, item){
+						frame_year=item.data_years;
 						if ((split_url=='index.html')&&(item.data_section=='portfolio')){
 							if((frame_year=='2016')&&(item.years_num!=='0')){
 								work_years=1;
@@ -630,8 +631,8 @@ $(function(){
 							}
 						}
 					});
-				// };
-				// contents_main();
+				};
+				contents_main();
 			});
 		}
 	};
@@ -656,7 +657,6 @@ $(function(){
 		}
 	};
 	function contents_box(){
-		frame_year=item.data_years;
 		$('.move-wrap'+work_years).append('<div class="scrollall-con-box" id="scroll'+work_years+'-con-box'+item.years_num+'"><div id="con-box'+work_years+'-imgborder'+item.years_num+'" class="con-box-imgborder">\
 		<img src="'+item.data_img+'" alt="'+item.data_alt+'"></div><div id="con-box'+work_years+'-caption'+item.years_num+'" class="con-box-caption">'+item.data_title+'</div></div>');
 	};
