b9cfb316590cfbb5c6d5179f6628a626e9e63db0
Lee.Hyunsup <clocktree@live.co.kr>
2020-11-02 20:09:01 +0900

    20201102_7
    - 수정.

--- js/move.js
+++ js/move.js
@@ -315,7 +315,7 @@ $(function(){
 			}
 			return false;
 		}
-		if (($('#article1-nav1-topmenu1').css('display')!='none')||($('body').css('width')>480))
+		if ($('#article1-nav1-topmenu1').css('display')!='none')
 		{
 			$('.body-footer-add').animate({'margin-left':'0px'},100)
 			$('.nav1-side-menu').animate({'right':'-100%'},100)
