01ef4867e38a1736eae3a79ad3632473dcb9d68c
Lee.Hyunsup <clocktree@live.co.kr>
2022-04-21 15:04:57 +0900

    20220421_1
    - 수정.

--- js/move.js
+++ js/move.js
@@ -615,8 +615,8 @@ $(function(){
 			$.each(data, function(I, item){
 				frame_year=item.data_years;
 				function contents_box(){
-					$('.move-wrap'+work_years).append('<div class="scrollall-con-box" id="scroll'+work_years+'-con-box'+item.years_num+'" tabindex="0"><div id="con-box'+work_years+'-imgborder'+item.years_num+'" class="con-box-imgborder">\
-					<img src="'+item.data_img+'" alt="'+item.data_alt+'"></div><div id="con-box'+work_years+'-caption'+item.years_num+'" class="con-box-caption"><span>'+item.data_title+'</span></div></div>');
+					$('.move-wrap'+work_years).append('<a class="scrollall-con-box" id="scroll'+work_years+'-con-box'+item.years_num+'" tabindex="0"><div id="con-box'+work_years+'-imgborder'+item.years_num+'" class="con-box-imgborder">\
+					<img src="'+item.data_img+'" alt="'+item.data_alt+'"></div><div id="con-box'+work_years+'-caption'+item.years_num+'" class="con-box-caption"><span>'+item.data_title+'</span></div></a>');
 				};
 				if (item.data_section=='portfolio'){
 					if((frame_year=='2016')&&(item.years_num!=='0')){
