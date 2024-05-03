fdba060057f77fd6aed206f62d49725e9343b8f6
Lee.Hyunsup <clocktree@live.co.kr>
2024-05-03 17:08:09 +0900

    20240503_5
    - 수정.

--- js/move.js
+++ js/move.js
@@ -49,7 +49,7 @@ $(function(){
 				$('.body-footer-contaner').prepend('<div id="body-footer-add1" class="body-footer-add"><address></address></div>')
 				$.each(data, function(I, item){
 					if(item.cover_kinds=='footer'){
-						$('.body-footer-contaner').find('address').append(item.cover_name+' <span class="footer-add-num">'+item.cover_phone+'</span><br>'+item.cover_address+'<br><a href="mailto:'+item.cover_email+'">'+item.cover_email+'</a><br><span class="Copyright">'+item.cover_Copyright+'</span><br>')
+						$('.body-footer-contaner').find('address').append(item.cover_name+' <span class="footer-add-num">'+item.cover_phone+'</span><br>'+item.cover_address+'<br><a href="mailto:'+item.cover_email+'" tabindex="0">'+item.cover_email+'</a><br><span class="Copyright">'+item.cover_Copyright+'</span><br>')
 					}
 				});
 			});
