31404dd294eb5a37fc2085601e73677fd8701662
Lee.Hyunsup <clocktree@live.co.kr>
2024-05-04 19:21:55 +0900

    20240504_13
    - 수정.

--- js/move.js
+++ js/move.js
@@ -891,12 +891,13 @@ $(function(){
 						console.log('pop ride this_2');
 						last_content.focus();
 					}
-					if ((e.keyCode===13)){
+					if ((e.keyCode===13)&&($(this).is('.all-filter-conbox'))){
 						// var link_a = $('.filter-conbox-contentswrap').children('.filter-conbox-contents').find('.contents-view-img').find('a').text();
 						// console.log('link_a = '+link_a);
 						// window.open("about:blank").location.href = link_a;
-						console.log('in a this ='+$(this).attr('class'));
-						$(this).click();
+						$('.filter-conbox-contentswrap').children('.filter-conbox-contents').find('.contents-view-img').find('a').focus();
+						// console.log('in a this ='+$(this).attr('href'));
+						// $(this).click();
 					}
 				}
 			});
