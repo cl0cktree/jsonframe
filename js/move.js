db725afad4c10156ec9bbade937369fbafc5720a
Lee.Hyunsup <clocktree@live.co.kr>
2024-05-04 18:25:36 +0900

    20240504_8
    - 수정.

--- js/move.js
+++ js/move.js
@@ -895,7 +895,8 @@ $(function(){
 						// var link_a = $('.filter-conbox-contentswrap').children('.filter-conbox-contents').find('.contents-view-img').find('a').text();
 						// console.log('link_a = '+link_a);
 						// window.open("about:blank").location.href = link_a;
-						$(this).find('a').click();
+						consolr.log('in a this ='+$(this).attr('href'));
+						$(this).click();
 					}
 				}
 			});
