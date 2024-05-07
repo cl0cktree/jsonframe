1cdec0d876c0ce4508d6d543975afb13a42f8dd5
Lee.Hyunsup <clocktree@live.co.kr>
2024-05-07 18:14:52 +0900

    20240507_4
    - 수정.

--- js/move.js
+++ js/move.js
@@ -946,6 +946,7 @@ $(function(){
 	function landing_contents(){
 		if(document.getElementById('filter-landing-contents')){
 			var filter_landing_contents = document.getElementById('filter-landing-contents');
+			console.log('landing focus on!');
 
 			filter_landing_contents.addEventListener('keydown', function(e){
 				var this_on_focus;
