0a2faefdf3a2bd2acfbb4fde2d4185988a8aca27
Lee.Hyunsup <clocktree@live.co.kr>
2024-11-19 16:05:45 +0900

    20241119_10
    - 수정.

--- js/move.js
+++ js/move.js
@@ -25,19 +25,19 @@ $(function(){
 	var split_url = this.location.href.split('/').reverse()[0];
 
 	function select_url(){
-		if((this_url.href=='http://www.clocktree.co.kr/')||(this_url.href=='http://www.clocktree.co.kr/index.html')){
+		if((this_url=='http://www.clocktree.co.kr/')||(this_url=='http://www.clocktree.co.kr/index.html')){
 			portfolioindex_url = 'http://www.clocktree.co.kr'
 			console.log('type_1 = '+portfolioindex_url);
-		}else if((this_url.href=='http://clocktreedomain.dothome.co.kr/')||(this_url.href=='http://clocktreedomain.dothome.co.kr/index.html')){
+		}else if((this_url=='http://clocktreedomain.dothome.co.kr/')||(this_url=='http://clocktreedomain.dothome.co.kr/index.html')){
 			portfolioindex_url = 'http://clocktreedomain.dothome.co.kr'
 			console.log('type_2 = '+portfolioindex_url);
-		}else if((this_url.href=='http://www.clocktree.kr/portfoliomain/')||(this_url.href=='http://www.clocktree.kr/portfoliomain/index.html')){
+		}else if((this_url=='http://www.clocktree.kr/portfoliomain/')||(this_url=='http://www.clocktree.kr/portfoliomain/index.html')){
 			portfolioindex_url = 'http://www.clocktree.kr/portfoliomain'
 			console.log('type_3 = '+portfolioindex_url);
-		}else if((this_url.href=='http://clocktree.dothome.co.kr/portfoliomain/')||(this_url.href=='http://clocktree.dothome.co.kr/portfoliomain/index.html')){
+		}else if((this_url=='http://clocktree.dothome.co.kr/portfoliomain/')||(this_url=='http://clocktree.dothome.co.kr/portfoliomain/index.html')){
 			portfolioindex_url = 'http://clocktree.dothome.co.kr/portfoliomain'
 			console.log('type_4 = '+portfolioindex_url);
-		}else if((this_url.href=='https://cl0cktree.github.io/jsonframe/')||(this_url.href=='https://cl0cktree.github.io/jsonframe/index.html')){
+		}else if((this_url=='https://cl0cktree.github.io/jsonframe/')||(this_url=='https://cl0cktree.github.io/jsonframe/index.html')){
 			portfolioindex_url = 'https://cl0cktree.github.io/jsonframe'
 			console.log('type_5 = '+portfolioindex_url);
 		};
