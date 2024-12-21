8eae05cfb4205868977442d07572376a14f678d5
Lee.Hyunsup <clocktree@live.co.kr>
2024-12-21 19:57:06 +0900

    20241221_4
    - 수정.

--- js/move.js
+++ js/move.js
@@ -358,9 +358,11 @@ $(function(){
 				$('#article1-nav1-topmenu1').find('.nav1-topmenu1-list').children('a').children('span').attr('aria-hidden','true');
 				$('footer').find('.menu-list-ul').children('.list-ul-li').children('a').attr('aria-hidden','false');
 				$('footer').find('.menu-list-ul').children('.list-ul-li').children('a').children('span').attr('aria-hidden','false');
-				$('#menu-icon input[type=checkbox]').prop('checked',true);
-				$('.nav1-side-menu').animate({'right':'0'},100);
-				$('footer').find('.menu-list-ul').children('.list-ul-li').children('a').focus();
+				if($('#article1-nav1-topmenu1').find('.nav1-topmenu1-list').children('a').focus()){
+					$('#menu-icon input[type=checkbox]').prop('checked',true);
+					$('.nav1-side-menu').animate({'right':'0'},100);
+					$('footer').find('.menu-list-ul').children('.list-ul-li').children('a').focus();
+				}
 				return false;
 			}
 			return false;
