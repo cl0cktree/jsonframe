2c9652392b9c3a0029e348f99dd3a142117f89a7
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-16 13:52:16 +0900

    20250116_1
    - 수정.

--- js/move.js
+++ js/move.js
@@ -1797,7 +1797,8 @@ $(function(){
 					weather_now = document.querySelector('.weather_now');
 					wearther_out = weather_now.innerHTML;
 					if((wearther_out=='Clear')&&((wearther_out!==null)||(wearther_out!==''))){
-						weather_Clear();
+						// weather_Clear();
+						weather_Clouds();
 					}else if((wearther_out=='Fine')&&((wearther_out!==null)||(wearther_out!==''))){
 						weather_Fine();
 					}else if((wearther_out=='Wind')&&((wearther_out!==null)||(wearther_out!==''))){
@@ -3061,7 +3062,7 @@ function weather_Wind() {
 	var b_canbus = document.querySelector('#click-all-filter-landing');
 	var circleArray = [];
 	var loopCancel;
-	var canvas = document.createElement('canvas');	
+	var canvas = document.createElement('canvas');
 	b_canbus.appendChild(canvas);
 	b_canbus.querySelector('canvas').classList.add('weather_canvas');
 	var weather_canvas = document.querySelector('.weather_canvas');
@@ -3542,34 +3543,41 @@ function weather_Clouds() {
 	/*#__PURE__*/
 	function () {
 		function Circle(info) {
-		_classCallCheck(this, Circle);
-
-		this.index = info.index;
-		this.x = info.x;
-		this.y = info.y;
-		this.speed = info.speed;
-		this.radius = info.radius;
-		this.startAngle = info.startAngle;
-		this.endAngle = info.endAngle;
-		this.clockwise = info.clockwise;
-		this.draw();
+			_classCallCheck(this, Circle);
+			this.index = info.index;
+			this.x = info.x;
+			this.y = info.y;
+			this.width = info.width;
+			this.height = info.height;
+			this.speed = info.speed;
+			this.radius = info.radius;
+			this.startAngle = info.startAngle;
+			this.endAngle = info.endAngle;
+			this.clockwise = info.clockwise;
+			this.draw();
 		}
 
 		_createClass(Circle, [{
 		key: "draw",
 		value: function draw() {
-			context.beginPath();
-			context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, toRadian(360), this.clockwise);
-			context.fillStyle = 'rgba(255, 255, 255, 0)';
-			context.fill();
-			context.strokeStyle = 'rgba(0, 0, 0, 0.15)';
-			context.lineWidth = '2';
-			context.stroke();
-			context.closePath();
-			context.fillStyle = '#fff';
-			//context.font = '30px bold sans-serif';
-			context.textAlign = "center";
-			//context.fillText(this.index, this.x, this.y+10);
+			var weather_img_src = portfolioindex_url+'/images/weather/weather_Clouds.png';
+			var weather_img = new Image();
+        	weather_img.src = weather_img_src;
+
+			// context.beginPath();
+			// context.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, toRadian(360), this.clockwise);
+			// context.fillStyle = 'rgba(255, 255, 255, 0)';
+			// context.fill();
+			// context.strokeStyle = 'rgba(0, 0, 0, 0.08)';
+			// context.lineWidth = '2';
+			// context.stroke();
+			// context.closePath();
+			// context.fillStyle = '#fff';
+			// context.font = '30px bold sans-serif';
+			// context.textAlign = "center";
+			// context.fillText(this.index, this.x, this.y+10);
+
+			context.drawImage(weather_img, this.x, this.y, this.width, this.height);
 			}
 		}]);
 
@@ -3587,13 +3595,16 @@ function weather_Clouds() {
 		var y;
 		var speed;
 		var circle;
+		var random_math;
 
-		for (var i = 0; i < 5; i++) {
+		for (var i = 0; i < 56; i++) {
 		x = Math.random() * window.innerWidth * 0.8 + Math.random() * window.innerWidth * 0.2;
 		y = Math.random() * window.innerHeight * 0.9; //높이 random생성.
 		//y = window.innerHeight+100; //처음부터 맨 아래에서 생성하고 싶은 경우 사용.
 
-		speed = Math.random() * 3 + 2;
+		speed = Math.random() * 2 + 1;
+		random_math = Math.floor(Math.random() * 100) + 100;
+
 		circle = new Circle({
 			index: i,
 			x: x,
@@ -3602,7 +3613,9 @@ function weather_Clouds() {
 			radius: Math.floor(Math.random() * 30) + 20,
 			startAngle: 360,
 			endAngle: 350,
-			clockwise: false
+			clockwise: false,
+			width: random_math,
+            height: random_math
 		});
 		circleArray.push(circle);
 		}
@@ -3616,21 +3629,22 @@ function weather_Clouds() {
 
 		for (var i = 0; i < circleArray.length; i++) {
 		circle = circleArray[i];
-		circle.y -= circle.speed; // 움직임을 아래에서 위로 주고 싶을 때 사용.
-		// circle.y += circle.speed; // 움직임을 위에서 아래로 주고 싶을 때 사용.
+		circle.x += circle.speed; // 움직임을 좌에서 우로 주고 싶을 때 사용.
+		// circle.y -= circle.speed; // 움직임을 아래에서 위로 주고 싶을 때 사용.
+		circle.y += circle.speed; // 움직임을 위에서 아래로 주고 싶을 때 사용.
 
 		// 움직임을 아래에서 위로 주고 싶을 때 사용.
-		if (circle.y < -circle.radius) {
-			circle.y = canvas.height;
-			circle.x = Math.random() * window.innerWidth * 0.8 + Math.random() * window.innerWidth * 0.2;
-			circle.radius = Math.floor(Math.random() * 30) + 20;
-		}
-		//  움직임을 위에서 아래로 주고 싶을 때 사용.
-		// if (circle.y > canvas.height) {
-		// 	circle.y = 0;
+		// if (circle.y < -circle.radius) {
+		// 	circle.y = canvas.height;
 		// 	circle.x = Math.random() * window.innerWidth * 0.8 + Math.random() * window.innerWidth * 0.2;
 		// 	circle.radius = Math.floor(Math.random() * 30) + 20;
 		// }
+		//  움직임을 위에서 아래로 주고 싶을 때 사용.
+		if (circle.x > canvas.width) {
+			circle.y = Math.random() * window.innerWidth * 0.1 + Math.random() * window.innerWidth * 0.1;
+			circle.x = 0;
+			circle.radius = Math.floor(Math.random() * 30) + 20;
+		}
 
 		circle.draw();
 		}
