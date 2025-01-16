d2a76e7a345a315868782ca71d986510f5a3edc4
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-16 16:10:58 +0900

    20250116_14
    - 수정.

--- js/move.js
+++ js/move.js
@@ -3861,13 +3861,13 @@ function weather_Hazy() {
 		var circle;
 		var random_math;
 
-		for (var i = 0; i < 32; i++) {
+		for (var i = 0; i < 24; i++) {
 		x = Math.random() * window.innerWidth * 0.8 + Math.random() * window.innerWidth * 0.2;
 		y = Math.random() * window.innerHeight * 0.9; //높이 random생성.
 		//y = window.innerHeight+100; //처음부터 맨 아래에서 생성하고 싶은 경우 사용.
 
 		speed = Math.random() * 0.8 + 1;
-		random_math = Math.floor(Math.random() * 500) + 400;
+		random_math = Math.floor(Math.random() * 600) + 500;
 
 		circle = new Circle({
 			index: i,
