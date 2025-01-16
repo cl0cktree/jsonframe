5f48a94afcca8c647fadcccd06a01fd21fb852ae
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-16 14:06:43 +0900

    20250116_4
    - 수정.

--- js/move.js
+++ js/move.js
@@ -3577,7 +3577,7 @@ function weather_Clouds() {
 			// context.textAlign = "center";
 			// context.fillText(this.index, this.x, this.y+10);
 
-			context.drawImage(weather_img, this.x, 0, this.width, this.height);
+			context.drawImage(weather_img, this.x, this.y, this.width, this.height);
 			}
 		}]);
 
@@ -3642,7 +3642,7 @@ function weather_Clouds() {
 		//  움직임을 위에서 아래로 주고 싶을 때 사용.
 		if (circle.x > canvas.width) {
 			circle.y = Math.random() * window.innerWidth * 0.1 + Math.random() * window.innerWidth * 0.1;
-			circle.x = 0;
+			circle.x = Math.random() * window.innerWidth * 0.1 + Math.random() * window.innerWidth * 0.1;
 			circle.radius = Math.floor(Math.random() * 30) + 20;
 		}
 
