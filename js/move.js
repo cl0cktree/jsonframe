cc2e9e202ce18997c923d6fe8249ac7f24759a51
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-30 19:09:16 +0900

    20250130_25
    - 수정.

--- js/move.js
+++ js/move.js
@@ -725,11 +725,13 @@ $(function(){
 
 					for (start_year=2016;start_year<=this_year;start_year++){
 						work_years++;
-						if(frame_year==start_year){
-							console.log('1 -- length = '+$('.main-scrollall-con').children('div').find('.scrollall-con-box').length);
-							contents_box();
-						}
-						if($('.main-scrollall-con').children('div').find('.scrollall-con-box').length==0){
+						
+						if($('.main-scrollall-con').children('div').find('.scrollall-con-box')){
+							if(frame_year==start_year){
+								console.log('1 -- length = '+$('.main-scrollall-con').children('div').find('.scrollall-con-box').length);
+								contents_box();
+							}
+						}else{
 							console.log('2 -- length = '+$('.main-scrollall-con').children('div').find('.scrollall-con-box').length);
 							empty_box();
 						}
