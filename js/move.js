e43a52be8cc1656a2f2a68be93f66cf1b8924441
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-30 16:03:08 +0900

    20250130_14
    - 수정.

--- js/move.js
+++ js/move.js
@@ -726,7 +726,7 @@ $(function(){
 					for (start_year=2016;start_year<=this_year;start_year++){
 						work_years++;
 						if(frame_year==start_year){
-							if(item.isNull(start_year)){
+							if(obj.has("data_years") && obj.isNull(start_year)){
 								empty_box();
 								console.log('--ride empty_box!! = '+frame_year);
 							}else{
