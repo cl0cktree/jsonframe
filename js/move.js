16c1c0ad9bf39ee6bfc64b372d2e917d33607124
Lee.Hyunsup <clocktree@live.co.kr>
2025-01-31 21:14:38 +0900

    20250131_31
    - 수정.

--- js/move.js
+++ js/move.js
@@ -864,7 +864,7 @@ $(function(){
 					$('.move-wrap'+work_years).append('<div class="scrollall-con-box" id="scroll'+work_years+'-con-box'+item.years_num+'" aria-haspopup="true" role="button" tabindex="0"><div id="con-box'+work_years+'-imgborder'+item.years_num+'" class="con-box-imgborder">\
 					<img src="'+item.data_img+'" alt="'+item.data_alt+'"></div><div id="con-box'+work_years+'-caption'+item.years_num+'" class="con-box-caption"><span>'+item.data_title+'</span></div></div>');
 					
-					console.log('-- empty this = '+this.ClassName);
+					console.log('-- empty this = '+this.tagName);
 					if($('.move-wrap'+work_years).find('.scrollall-con-box').attr('data-empty','true').length>0){
 						console.log('-- empty length = '+$('.move-wrap'+work_years).find('.scrollall-con-box').attr('data-empty','true').length);
 						$(this).remove();
