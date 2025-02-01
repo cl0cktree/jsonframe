a9dcdec527abbba399e846772a61e76fd32ba712
Lee.Hyunsup <clocktree@live.co.kr>
2025-02-01 15:01:02 +0900

    20250201_16
    - 수정.

--- js/move.js
+++ js/move.js
@@ -4082,7 +4082,7 @@ function weather_Mist() {
 		// }
 		//  움직임을 위에서 아래로 주고 싶을 때 사용.
 		if (circle.y < -circle.radius) {
-			circle.y = canvas.height*0.2;
+			circle.y = canvas.height*0;
 			circle.x = 0;
 			circle.radius = Math.floor(Math.random() * 30) + 20;
 		}
