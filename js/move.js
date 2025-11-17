fdd373150f331019444af9c8d3efae5c4f936f8d
Lee.Hyunsup <clocktree@live.co.kr>
2025-11-17 19:33:21 +0900

    20251117_30
    - 수정.

--- js/move.js
+++ js/move.js
@@ -1334,7 +1334,7 @@ $(function(){
 	/*전체 마우스휠 이벤트 발생 시 스크롤 이동 속도 제어*/
 	function wheel_con(event){		
 		var prevScroll = 0;		
-		$('body, html').on('mousewheel', '.body-section-content', function(event){			
+		$('body, html').on('mousewheel', , {passive: false}, '.body-section-content', function(event){			
 			console.log('this event = '+this,'events');
 			// if(event.type=='mousewheel'){
 			// 	event.preventDefault();
