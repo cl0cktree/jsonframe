2225398e39633773d68a762097e62fa884857a3e
Lee.Hyunsup <clocktree@live.co.kr>
2025-11-18 17:25:06 +0900

    20251118_27
    - 수정.

--- js/move.js
+++ js/move.js
@@ -1360,16 +1360,16 @@ $(function(){
 	$('body, html').on('wheel mousewheel DOMMouseScroll', '.body-section-content', function(event){
 		// event.preventDefault();
 		var wheel_delay_time_1;
-		var wheel_delay = 145;
+		var wheel_delay = 150;
 		var wheel_range = 30;
-		var wheel_speed = wheel_range*4;
+		var wheel_speed = wheel_range*2;
 		
 		if(event.originalEvent.wheelDelta >= 0){	// 휠 업다운에 따라 음수와 양수 값을 내장 함수에서 직접 받아옴.		
 			if(!wheel_delay_time_1){
 				wheel_delay_time_1 = setTimeout(function(){
 					wheel_delay_time_1=null;
 					$('body, html').stop().animate({scrollTop: $(window).scrollTop()-wheel_range},wheel_speed, function(){
-						$('body, html').stop().animate({scrollTop: $(window).scrollTop()+wheel_range},wheel_speed/2)
+						$('body, html').stop().animate({scrollTop: $(window).scrollTop()+wheel_range/2},wheel_speed/2)
 					});
 					console.log('+++ wheel_con() is plus = '+$(window).scrollTop());
 				},wheel_delay)
@@ -1379,7 +1379,7 @@ $(function(){
 				wheel_delay_time_1 = setTimeout(function(){
 					wheel_delay_time_1=null;
 					$('body, html').stop().animate({scrollTop: $(window).scrollTop()+wheel_range},wheel_speed, function(){
-						$('body, html').stop().animate({scrollTop: $(window).scrollTop()-wheel_range},wheel_speed/2)
+						$('body, html').stop().animate({scrollTop: $(window).scrollTop()-wheel_range/2},wheel_speed/2)
 					});
 					console.log('--- wheel_con() is minus = '+$(window).scrollTop());
 				},wheel_delay)
