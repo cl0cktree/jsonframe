5fd9166c250709dcdf57b88341f2eb59499c2155
Lee.Hyunsup <clocktree@live.co.kr>
2026-01-07 16:05:00 +0900

    20260107_1
    - 수정.

--- js/move.js
+++ js/move.js
@@ -360,8 +360,8 @@ $(function(){
 				flashSommon();
 			}else if (split_url=='sub5.html'){
 				// imageSommon();
-				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>')
-				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>');
+				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');
+				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');;
 				// PageSommon(head_this, h2_this, data_kind);
 			}
 			$('.section-heading').html(contents_head);
@@ -643,8 +643,8 @@ $(function(){
 			{
 				nav_num = 5;
 				// imageSommon();
-				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>')
-				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>');
+				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');
+				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');;
 				// PageSommon(head_this, h2_this, data_kind);
 			}
 			title_changer();
@@ -700,8 +700,8 @@ $(function(){
 			{
 				nav_num = 5;
 				// imageSommon();
-				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>')
-				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>');
+				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');
+				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');;
 				// PageSommon(head_this, h2_this, data_kind);
 			}
 			title_changer();
@@ -1177,7 +1177,7 @@ $(function(){
 	var web_summ = new PageSommon('웹작업페이지 콘텐츠 묶음','web','<h2>웹&앱 퍼블리싱</h2>');
 	var movie_summ = new PageSommon('동영상작업페이지 콘텐츠 묶음','movie','<h2>동영상 편집작업</h2>');
 	var flash_summ = new PageSommon('플래시작업페이지 콘텐츠 묶음','flash','<h2>플래시</h2>');
-	var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>');
+	var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');
 	/*------------------------------------------------------------------*/
 	/*-모달로그 창 위 컨텐츠의 포커스 요소만 순회하는 함수+esc키 눌렀을 때 모달로그 닫히기+낮은 단계의 레이어 선택 요소에 포커스 유지-*/
 	function conbox_contents(){ // 높은 단계의 모달로그 컨텐츠의 포커스 요소만 순회 함수 + esc 키 눌렀을 시 닫히기
@@ -1367,8 +1367,8 @@ $(function(){
 				$('#slide-wrap').css({'display':'block'})
 				$('#slide-wrap-i').css({'display':'none'})
 				// imageSommon();
-				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>')
-				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','image','<h2>2D랜더링&편집작업</h2>');
+				new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');
+				// var image_summ = new PageSommon('랜더링및편집작업페이지 콘텐츠 묶음','<h2>2D랜더링&편집작업</h2>','image');;
 				// PageSommon(head_this, h2_this, data_kind);
 			}
 			title_changer();
