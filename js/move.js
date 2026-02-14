17bc8bf81708163d14de8e82036826c2bb8cfe59
Lee.Hyunsup <clocktree@live.co.kr>
2026-02-14 18:52:10 +0900

    20260214_4
    - 수정.

--- js/move.js
+++ js/move.js
@@ -2041,10 +2041,9 @@ $(function(){
 		$('.click-all-filter').fadeIn('fast',function(){
 			$('.click-all-filter').html('<div id="all-filter-conbox" class="all-filter-conbox"><button type="button" id="filter-title-closebtn" class="filter-title-closebtn" tabindex="0"><img src="'+portfolioindex_url+'/images/closebtn.png" alt="결과물 자세히보기 종료"></button><div id="filter-conbox-contentswrap" class="filter-conbox-contentswrap"></div></div>');
 			// $('.filter-conbox-contentswrap').html('<iframe src="'+portfolioindex_url+'/sub/email.html" width="100%" height="485px" frame-border="0" marginheight="0" marginwidth="0" scrolling="yes"></iframe>');
-			$('.filter-conbox-contentswrap').load(portfolioindex_url+'/sub/email.html .email_wrap');
 			$.getScript(portfolioindex_url+'/js/emailset.js', function() {
 				console.log("스크립트 로드 완료 및 실행");
-				// 스크립트 로드 후 필요한 함수 실행
+				$('.filter-conbox-contentswrap').load(portfolioindex_url+'/sub/email.html .email_wrap');
 			});
 			conbox_contents();
 		});
